81272a1fe3fbbccec857f1e42326677b52c8507b
lapon <pranajatin@gmail.com>
2025-09-17 13:34:15 +0700

    first commit

---
+++ database/seeders/KategoriSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class KategoriSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('kategoris')->insert([
+            [
+                'nama_kategori' => 'Elektronik',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama_kategori' => 'Mebel & Furnitur',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama_kategori' => 'Alat Tulis Kantor (ATK)',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama_kategori' => 'Aset Gedung',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
---
+++ resources/views/components/progress-kondisi.blade.php
@@ -0,0 +1,19 @@
+@props(['judul', 'jumlah', 'kondisi', 'color'])
+
+<h4 class="small font-weight-bold">
+    {{ $judul }}
+    <span class="float-end">{{ $kondisi }}</span>
+</h4>
+
+<div class="progress mb-4">
+    <div 
+        class="progress-bar bg-{{ $color }}" 
+        role="progressbar"
+        style="width: {{ $jumlah > 0 ? ($kondisi / $jumlah) * 100 : 0 }}%;"
+        aria-valuenow="{{ $jumlah > 0 ? ($kondisi / $jumlah) * 100 : 0 }}"
+        aria-valuemin="0" 
+        aria-valuemax="100"
+    >
+        {{ $jumlah > 0 ? round(($kondisi / $jumlah) * 100) : 0 }}%
+    </div>
+</div>
